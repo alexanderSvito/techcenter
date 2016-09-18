41437c1c1315d64f6d5227a2e7facbaee27dab0e
Hesoyam <alexandervirk@gmail.com>
2016-09-18 17:51:37 +0300

    Update app_controller.rb

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,8 +1,8 @@
 class AppController < ApplicationController
 	
 	def home
-	cookies.permanent[:stgr_cart] = Cart.create!
-	cookies.permanent[:log_cart] = Cart.create!(user_id: @user.id).id if logged_in?	
+	cookies.permanent[:stgr_cart] ||= Cart.create!.id
+	cookies.permanent[:log_cart] ||= Cart.create!(user_id: @user.id).id if logged_in?	
   	@cheap = Item.find_by_sql('SELECT * FROM items i ORDER BY price LIMIT 4').map! { |x| x = { id: x.id, name: x.name, price: x.price, link: x.avatar.url, rate: x.calculate_rate} }
   	@visited = Item.find_by_sql('SELECT * FROM items LIMIT 4').map! { |x| x = { id: x.id, name: x.name, price: x.price, link: x.avatar.url, rate: x.calculate_rate} }
   	@top = Item.find_by_sql('SELECT * FROM items LIMIT 4').map! { |x| x = { id: x.id, name: x.name, price: x.price, link: x.avatar.url, rate: x.calculate_rate} }
